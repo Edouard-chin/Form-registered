6356a9864d82008e56ee87bf6d6aa02cecd81825
Edouard CHIN <echin@u-pro.fr>
2015-02-21 03:06:23 +0100

    Cleaning, reorganizing template. Ready to work

---
+++ src/Dudek/FormBundle/Controller/Front/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dudek\FormBundle\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+    public function registerAction(Request $request)
+    {
+    }
+}
--- src/Dudek/UserBundle/Controller/DefaultController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dudek\UserBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class DefaultController extends Controller
-{
-}
